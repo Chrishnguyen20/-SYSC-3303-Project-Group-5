23b60b032d46baf965836ee42764e2aaf0496c62
FMahamud <91301445+FMahamud@users.noreply.github.com>
2022-01-25 12:33:45 -0500

    Add base classes with skeleton code

---
+++ src/Scheduler.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Scheduler{
+	
+	private ArrayList<String> queue;
+	
+	public Scheduler() {
+		queue = new ArrayList<>();
+	}
+	
+	public synchronized void recieveUpdate(String info) {
+		queue.add(info);
+	}
+	
+	public synchronized String updateElevator() {
+		try {
+			String element = queue.remove(0);
+			return element;
+		} catch (NullPointerException e) {
+			return null;
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+		Scheduler scheduler = new Scheduler();
+		Elevator elevator = new Elevator("Elevator", scheduler);
+		
+	}
+
+}
