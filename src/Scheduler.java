777193b9834931feb62488a6d73714118ce33db4
minhj <minhj@DESKTOP-EL6C87I>
2022-01-30 13:57:34 -0500

    Created Main Class

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -56,15 +56,4 @@ public class Scheduler{
 			return null;
 		}
 	}
-	
-	public static void main(String args[]) {
-		
-		Scheduler scheduler = new Scheduler();
-		Elevator elevator = new Elevator("Elevator", scheduler);
-		Thread floor = new Thread(new Floor("Floor", scheduler), "Floor Thread");
-
-	
-		floor.start();
-	}
-
 }
