0a2a5345a0823fb4c643acf8a56b10e59d804520
minhj <minhj@DESKTOP-EL6C87I>
2022-02-02 19:39:48 -0500

    Modified Scheduler elevatorUpdate

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -12,15 +12,17 @@ public class Scheduler{
 	
 	private ArrayList<FloorRequest> fr;
 	private ArrayList<Elevator> elevators;
+	private ArrayList<Elevator> idleElevators;
 
 	private boolean newFloorRequest;
-	private boolean pendingFloorRequest;
 	
 	public Scheduler() {
 		this.fr = new ArrayList<>();
 
 		this.newFloorRequest = true;
-		this.pendingFloorRequest = false;
+	}
+	
+	public void initElevators(int count) {
 	}
 	
 	
@@ -29,19 +31,18 @@ public class Scheduler{
 	 * @param request - a new floor request object
 	 */
 	public synchronized void floorRequest(FloorRequest request) {
-		while(!this.newFloorRequest) {
-			try {
-				wait();
-			}catch( InterruptedException e) {
-				System.err.println(e);
-			}
-		}
+//		while(!this.newFloorRequest) {
+//			try {
+//				wait();
+//			}catch( InterruptedException e) {
+//				System.err.println(e);
+//			}
+//		}
 		
 		this.fr.add(request);
 			
 		System.out.println(request.toString());
-		//TODO: process the event in some way
-		this.pendingFloorRequest = true;
+
 		notifyAll();
 	}
 	
@@ -56,6 +57,7 @@ public class Scheduler{
 		
 		elevator.setDestFloor(fr.get(0).getFloorNum());
 		fr.remove(0);
+		elevator.startCar();
 			
 		System.out.println(elevator.toString());
 		notifyAll();
@@ -63,15 +65,10 @@ public class Scheduler{
 	
 	public synchronized void elevatorUpdate(int carNum, int curFloor) {
 		int dest = elevators.get(carNum).getDestFloor();
-		if (curFloor > dest) {
-			curFloor--;
-		}
-		else if (curFloor < dest) {
-			curFloor++;
-		}
 		elevators.get(carNum).setCurrentFloor(curFloor);
 		if (curFloor == dest) {
 			elevators.get(carNum).setIdle();
+			notifyAll();
 		}
 	}
 }
