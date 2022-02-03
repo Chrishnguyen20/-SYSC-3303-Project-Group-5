28663cf3da9fb1d8da23526c34609053978d4cbd
Bghadie <billalkghadie@hotmail.com>
2022-02-02 21:15:51 -0500

    Major refactoring

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -6,70 +6,77 @@ import java.util.ArrayList;
  */
 
 
-public class Scheduler{
+public class Scheduler implements Runnable{
 	
 	// TO-DO
 	
-	private ArrayList<FloorRequest> fr;
-	private ArrayList<Elevator> elevators;
+	private FloorRequest floorRequest;
 
-	private boolean newFloorRequest;
-	private boolean pendingFloorRequest;
+	private ElevatorRequest elevatorRequest;
 	
-	public Scheduler() {
-		this.fr = new ArrayList<>();
+	public Scheduler(FloorRequest fr) {
+		this.floorRequest = fr;
+	}
 
-		this.newFloorRequest = true;
-		this.pendingFloorRequest = false;
-	}
 	
-	public void initElevator(int num) {
-		//for(int i = 0; i < )
-	}
-	
-	/*
-	 * @purpose       - adds a new floor request to the queue for the scheduler to process
-	 * @param request - a new floor request object
-	 */
-	public synchronized void floorRequest(FloorRequest request) {
-		while(!this.newFloorRequest) {
-			try {
-				wait();
-			}catch( InterruptedException e) {
-				System.err.println(e);
+	public void run() {
+		while(true) {
+			while(this.floorRequest.hasRequest()) {
+				elevatorRequest.notifyElevatorRequest(floorRequest.getCarBut());
+				
+				elevatorRequest.requestServed();
+				
+				floorRequest.remove();
+			
 			}
 		}
-		
-		this.fr.add(request);
-			
-		System.out.println(request.toString());
-		//TODO: process the event in some way
-		this.pendingFloorRequest = true;
-		notifyAll();
 	}
 	
-	public synchronized void elevatorRequest(Elevator elevator) {
-		while(fr.isEmpty()) {
-			try {
-				wait();
-			}catch( InterruptedException e) {
-				System.err.println(e);
-			}
-		}
-		
-		elevator.setDestFloor(fr.get(0).getCarBut());
-		fr.remove(0);
-			
-		System.out.println(elevator.toString());
-		notifyAll();
-	}
 	
-	public synchronized void elevatorUpdate(int carNum, int curFloor) {
-		
-		int dest = elevators.get(carNum).getDestFloor();
-		elevators.get(carNum).setCurrentFloor(curFloor);
-		if (curFloor == dest) {
-			elevators.get(carNum).setIdle();
-		}
-	}
+//	
+//	/*
+//	 * @purpose       - adds a new floor request to the queue for the scheduler to process
+//	 * @param request - a new floor request object
+//	 */
+//	public synchronized void floorRequest(FloorRequest request) {
+//		while(!this.newFloorRequest) {
+//			try {
+//				wait();
+//			}catch( InterruptedException e) {
+//				System.err.println(e);
+//			}
+//		}
+//		
+//		this.fr.add(request);
+//			
+//		System.out.println(request.toString());
+//		//TODO: process the event in some way
+//		this.pendingFloorRequest = true;
+//		notifyAll();
+//	}
+//	
+//	public synchronized void elevatorRequest(Elevator elevator) {
+//		while(fr.isEmpty()) {
+//			try {
+//				wait();
+//			}catch( InterruptedException e) {
+//				System.err.println(e);
+//			}
+//		}
+//		
+//		elevator.setDestFloor(fr.get(0).getCarBut());
+//		fr.remove(0);
+//			
+//		System.out.println(elevator.toString());
+//		notifyAll();
+//	}
+//	
+//	public synchronized void elevatorUpdate(int carNum, int curFloor) {
+//		
+//		int dest = elevators.get(carNum).getDestFloor();
+//		elevators.get(carNum).setCurrentFloor(curFloor);
+//		if (curFloor == dest) {
+//			elevators.get(carNum).setIdle();
+//		}
+//	}
 }
