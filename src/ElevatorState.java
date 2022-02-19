00b1c8a95bc18a91c273378456ae8e4f52f4948a
minhj <minhj@DESKTOP-EL6C87I>
2022-02-18 20:23:42 -0500

    Reformatted Elevator class

--- src/ElevatorState.java
+++ src/ElevatorState.java
@@ -1,72 +1,71 @@
 
 public enum ElevatorState {
 
-    Initial {
-        @Override
-        public ElevatorState nextState(Elevator elevator) {
-        	
-            return NoElevatorRequest;
-        }
+	Initial {
+		@Override
+		public ElevatorState nextState(Elevator elevator) {
+			return NoElevatorRequest;
+		}
 
-        @Override
-        public String getElevatorState() {
-            return "Initial";
-        }
-    },
-    NoElevatorRequest {
-        @Override
-        public ElevatorState nextState(Elevator elevator) {
-        	if (elevator.elevatorRequest.hasRequest()) {
-        		return MoveToDestination;
-        	}
-            return NoElevatorRequest;
-        }
+		@Override
+		public String getElevatorState() {
+			return "Initial";
+		}
+	},
+	NoElevatorRequest {
+		@Override
+		public ElevatorState nextState(Elevator elevator) {
+			if (elevator.getElevatorRequest().hasRequest()) {
+				return MoveToDestination;
+			}
+			return NoElevatorRequest;
+		}
 
-        @Override
-        public String getElevatorState() {
-            return "NoElevatorRequest";
-        }
-    },
-    MoveToDestination {
-        @Override
-        public ElevatorState nextState(Elevator elevator) {
-        	if (elevator.receivedPassengers == 0 && elevator.currentFloor == elevator.getFloorNum()) {
-        		return PassengersBoarding;
-        	}
-        	else if (elevator.elevatorRequest.hasArrived()) {
-        		return HasArrived;
-        	}
-            return MoveToDestination;
-        }
+		@Override
+		public String getElevatorState() {
+			return "NoElevatorRequest";
+		}
+	},
+	MoveToDestination {
+		@Override
+		public ElevatorState nextState(Elevator elevator) {
+			if (elevator.getReceivedPassengers() == 0 && elevator.getCurrentFloor() == elevator.getFloorNum()) {
+				return PassengersBoarding;
+			} else if (elevator.getElevatorRequest().hasArrived()) {
+				return HasArrived;
+			}
+			return MoveToDestination;
+		}
 
-        @Override
-        public String getElevatorState() {
-            return "MoveToDestination";
-        }
-    },
-    PassengersBoarding {
-        @Override
-        public ElevatorState nextState(Elevator elevator) {
-            return MoveToDestination;
-        }
+		@Override
+		public String getElevatorState() {
+			return "MoveToDestination";
+		}
+	},
+	PassengersBoarding {
+		@Override
+		public ElevatorState nextState(Elevator elevator) {
+			return MoveToDestination;
+		}
 
-        @Override
-        public String getElevatorState() {
-            return "PassengersBoarding";
-        }
-    },
-    HasArrived {
-        @Override
-        public ElevatorState nextState(Elevator elevator) {
-            return NoElevatorRequest;
-        }
+		@Override
+		public String getElevatorState() {
+			return "PassengersBoarding";
+		}
+	},
+	HasArrived {
+		@Override
+		public ElevatorState nextState(Elevator elevator) {
+			return NoElevatorRequest;
+		}
 
-        @Override
-        public String getElevatorState() {
-            return "HasArrived";
-        }
-    };
+		@Override
+		public String getElevatorState() {
+			return "HasArrived";
+		}
+	};
 
-    public abstract ElevatorState nextState(Elevator elevator); 
-    public abstract String getElevatorState();
+	public abstract ElevatorState nextState(Elevator elevator);
+
+	public abstract String getElevatorState();
 }
