e76b922b13865ac86451c17caaa700069fc67de5
minhj <minhj@DESKTOP-EL6C87I>
2022-03-27 17:20:08 -0400

    Still broken, fixed Scheduler getAvailableElevator()

--- src/Elevator.java
+++ src/Elevator.java
@@ -109,7 +109,9 @@ public class Elevator implements Runnable {
 		
 	public String getState() { return state.getElevatorState(); }
 	
-	public boolean hasRequest() { return this.hasRequest; }
+	//public boolean hasRequest() { return this.hasRequest; }
+	
+	public boolean hasRequest() { return !this.destFloors.isEmpty(); }
 	
 	public boolean checkFaulted() { return this.hasFaulted; }
 	
